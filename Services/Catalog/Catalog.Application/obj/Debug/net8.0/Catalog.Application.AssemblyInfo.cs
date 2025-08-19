428a9ea465a5281bd5a4ad49694ee7f7d0c852d6
Trường Thịnh <nguyentruongthinh17022003@gmail.com>
2025-08-19 15:33:49 +0700

    add data seed and context them to import db

--- Services/Catalog/Catalog.Application/obj/Debug/net8.0/Catalog.Application.AssemblyInfo.cs
+++ Services/Catalog/Catalog.Application/obj/Debug/net8.0/Catalog.Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Catalog.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b554fba6e0875bbb6fe43503618634a2602c233b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Catalog.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Catalog.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
